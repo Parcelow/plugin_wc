1bbfbfa7944f4b8f4ff129411dd80e741601a986
AAT2022 <annyangelis@hotmail.com>
2022-11-04 16:02:19 -0300

    2.9.6

--- src/woo-parcelow.php
+++ src/woo-parcelow.php
@@ -6,7 +6,7 @@
  * Description: Take credit card payments on your store using Parcelow.
  * Author: Parcelow
  * Author URI: https://parcelow.com/
- * Version: 2.9.4
+ * Version: __STABLE_TAG__
  * Requires at least: 5.9
  * Tested up to: 5.9.3
  * WC requires at least: 6.3.1
